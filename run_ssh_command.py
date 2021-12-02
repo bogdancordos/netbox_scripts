ef632cc52eaf2767429d997b6f6422bcc9c1b8a2
Bogdan Cordos <57483807+bogdancordos@users.noreply.github.com>
2021-12-02 10:52:06 +0000

    Update run_ssh_command.py
    Import and use NAPALM Credentials.

--- run_ssh_command.py
+++ run_ssh_command.py
@@ -1,6 +1,7 @@
 #add netmiko to the local_requirements.txt
 from django.utils.text import slugify
 from netmiko import ConnectHandler
+import netbox.settings
 from django.forms import PasswordInput
 from extras.scripts import *
 
@@ -9,17 +10,11 @@ class RunCommand(Script):
     class Meta:
         name = "Run command on Cisco Devices via SSH"
         description = "Run command on Cisco Devices via SSH"
-        field_order = ['input_ip', 'input_username', 'input_pass', 'input_command']
+        field_order = ['input_ip', 'input_command']
 
 
     input_ip = StringVar(
         description="Enter the IP Address:"
-    )
-    input_username = StringVar(
-        description="Enter username:"
-    )
-    input_pass = StringVar(
-        widget=PasswordInput
     )
     input_command = StringVar(
         description="Enter the SSH command:"
@@ -30,8 +25,8 @@ class RunCommand(Script):
         cisco1 = { 
             "device_type": "cisco_ios",
             "host": data['input_ip'],
-            "username": data['input_username'],
-            "password": data['input_pass'],
+            "username": netbox.settings.NAPALM_USERNAME,
+            "password": netbox.settings.NAPALM_PASSWORD,
         }
 
         command = data['input_command']
