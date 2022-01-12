a3e0ffd98ab38590a0b501b87add9d1a0e664617
Bogdan Cordos <57483807+bogdancordos@users.noreply.github.com>
2022-01-12 14:34:09 +0000

    Update whois.py

--- whois.py
+++ whois.py
@@ -24,10 +24,10 @@ class RunCommand(Script):
         rawoutput = response.text
         output = json.loads(rawoutput)
         
-#
-#Available fields:ip,success,type,continent,continent_code,country,country_code,country_flag,country_capital,country_phone,country_neighbours,
-#                 region,city,latitude,longitude,asn,org,isp,timezone,timezone_name,timezone_dstOffset,timezone_gmtOffset,timezone_gmt,currency
-#                 currency_code,currency_symbol,currency_rates,currency_plural,completed_requests
+        #
+        #Available fields:ip,success,type,continent,continent_code,country,country_code,country_flag,country_capital,country_phone,country_neighbours,
+        #                 region,city,latitude,longitude,asn,org,isp,timezone,timezone_name,timezone_dstOffset,timezone_gmtOffset,timezone_gmt,currency
+        #                 currency_code,currency_symbol,currency_rates,currency_plural,completed_requests
         
         self.log_success(f"IP :" + output['ip'])
         self.log_success(f"Type :" + output['type'])
