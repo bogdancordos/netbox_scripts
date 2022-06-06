403a5e4fefa1935d049158c011f38bc133066f67
Bogdan Cordos <57483807+bogdancordos@users.noreply.github.com>
2022-06-06 12:33:17 +0100

    Update run_ssh_command.py

--- run_ssh_command.py
+++ run_ssh_command.py
@@ -1,7 +1,9 @@
 #add netmiko to the local_requirements.txt
 from django.utils.text import slugify
 from netmiko import ConnectHandler
-import netbox.settings
+from django.forms import PasswordInput
+from dcim.models import Device, Interface
+from ipam.models import IPAddress
 from extras.scripts import *
 
 
@@ -9,23 +11,36 @@ class RunCommand(Script):
     class Meta:
         name = "Run command on Cisco Devices via SSH"
         description = "Run command on Cisco Devices via SSH"
-        field_order = ['input_ip', 'input_command']
+        field_order = ['device', 'input_username', 'input_pass', 'input_command']
 
 
-    input_ip = StringVar(
-        description="Enter the IP Address:"
+    device = ObjectVar(
+        model=Device,
+        query_params={
+            'status' : 'active'
+        }
+    )
+    input_username = StringVar(
+        description="Enter username:"
+    )
+    input_pass = StringVar(
+        widget=PasswordInput
     )
     input_command = StringVar(
         description="Enter the SSH command:"
     )
 
+
     def run(self, data, commit):
 
-        cisco1 = { 
+        address = data["device"].primary_ip
+        device_ip = str(address.address.ip)
+
+        cisco1 = {
             "device_type": "cisco_ios",
-            "host": data['input_ip'],
-            "username": netbox.settings.NAPALM_USERNAME,
-            "password": netbox.settings.NAPALM_PASSWORD,
+            "host": device_ip,
+            "username": data['input_username'],
+            "password": data['input_pass'],
         }
 
         command = data['input_command']
@@ -33,5 +48,7 @@ class RunCommand(Script):
         with ConnectHandler(**cisco1) as net_connect:
             output = net_connect.send_command(command)
 
+
+
             self.log_success("View the information bellow")
-        return ''.join(output)
+        return '' .join(output)
