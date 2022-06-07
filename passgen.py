71454ef207c8f7e993f27b88e28cd60c8c044b67
Bogdan Cordos <57483807+bogdancordos@users.noreply.github.com>
2022-06-07 10:20:28 +0100

    Update passgen.py

--- passgen.py
+++ passgen.py
@@ -6,21 +6,15 @@ from extras.scripts import *
 class PassGen(Script):
     class Meta:
         name = "Password Generator"
-        description = "Generate 5 passwords, 16 characters long "
+        description = "Generate n passwords, y characters long "
 
     def run(self, data, commit):
         characters = string.ascii_letters + string.digits
-        password01 =  "".join(choice(characters) for x in range(randint(16, 16)))
-        password02 =  "".join(choice(characters) for x in range(randint(16, 16)))
-        password03 =  "".join(choice(characters) for x in range(randint(16, 16)))
-        password04 =  "".join(choice(characters) for x in range(randint(16, 16)))
-        password05 =  "".join(choice(characters) for x in range(randint(16, 16)))
+        password01 =  "".join(choice(characters) for x in range(randint(y, y)))
 
-        passwords = [password01, password02, password03, password04, password05]
+
+        passwords = [password01]
         output=('\n'.join(map(str, passwords)))
         self.log_success(f"Password one:   {password01}")
-        self.log_success(f"Password two:   {password02}")
-        self.log_success(f"Password three: {password03}")
-        self.log_success(f"Password four:  {password04}")
-        self.log_success(f"Password five:  {password05}")
+
         return ('\n'.join(map(str, passwords)))
